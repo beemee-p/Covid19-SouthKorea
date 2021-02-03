f4ca4095694b005de70cd89e54dfe7156ae36f94
beemee-p <raina.y.park@gmail.com>
2021-02-04 04:27:46 +0900

    Add ArticleAPITest Branch and request API

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,18 +2,19 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Article = () => {
-  
-  const [articles, setArticles] = useState({}); 
+
+  const [articles, setArticles] = useState(); 
   //const [loading, setLoading] = useState(false);
   
   useEffect(() => {
     const fetchEvents = async ()=>{ 
+      //client 키 
       const client_id = 'ICVA0EEW_FBpV_N5tLOw';
       const client_secret = 'pbG8d9DZ3a';
       
       //setLoading(true);
       try {
-        const res = await axios.get("https://openapi.naver.com/v1/search/news.json", { 
+        const res = await axios.get("/v1/search/news.json", { 
           params:{
             query: '코로나확진자',
             display: 20
@@ -24,26 +25,26 @@ const Article = () => {
           } 
         });
         setArticles(res.data);
+        console.log(articles);
+        console.log("통과");
+       
       } catch (e) {
         console.log(e);
       }
      //setLoading(false);
     };
     fetchEvents();
+   
   }, []);
+
 
   return (
     <div>
-      <h2>코로나 관련 뉴스</h2>
-      <ul className="news">
-        {/* {articles.map((item) => (
-          <li>
-            <a target="_blank" href={item.link}>
-              {item.title}
-            </a>
-            <p>{item.description}</p>
-          </li>
-        ))} */}
+      <h2>코로나 관련 뉴스</h2> 
+      <ul className="news">   
+      {/* {articles.map(a => (
+       <li>{a.title}</li>       
+      ))} */}
       </ul>
     </div>
   )
